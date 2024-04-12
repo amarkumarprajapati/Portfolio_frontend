697993a2727af9e8de572ef7da947927a6eef381
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2024-04-12 10:03:25 +0530

    updated

--- src/Contailer/Blogs.jsx
+++ src/Contailer/Blogs.jsx
@@ -15,48 +15,71 @@ function Blogs() {
         </div>
       </div>
       <div className=" flex gap-10 justify-center py-16">
-        <div className="image h-[350px] rounded-xl w-96 justify-center items-center">
+        {/* 1 */}
+
+        <div className="group image h-[350px] rounded-xl w-96 justify-center items-center">
           <button className="text-white bg-blue-800 px-3 py-1 m-5 rounded-2xl">
             TUTORIAL
           </button>
-          <div className="bg-white hover:bg-violet-600 hover:text-white ease-in-out w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto  mt-44 duration-300 cursor-pointer">
-            <div className="flex font-semibold">
-              <img src="" alt="" />
+          <div className="bg-white group-hover:bg-violet-600 group-hover:text-white ease-in-out duration-300 delay-100 w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto mt-40 cursor-pointer ">
+            <div className="flex font-semibold items-center gap-2">
+              <img src="/src/Assects/images/clock.png" className="w-5" alt="" />
               <p>Date</p>
-              <img src="" alt="" />
-              <p>comment</p>
+              <div className="flex font-semibold items-center py-1 gap-2">
+                <img
+                  src="/src/Assects/images/comments.png"
+                  alt=""
+                  className="w-5"
+                />
+                <p>comment</p>
+              </div>
             </div>
             <div className="text-xl font-bold">
               <p>TOP 10 Ui Ux Designers</p>
             </div>
           </div>
         </div>
-        <div className="image1 h-[350px] rounded-xl w-96 justify-center items-center">
+
+        {/* 2 */}
+        <div className="group image1 h-[350px] rounded-xl w-96 justify-center items-center">
           <button className="text-white bg-blue-800 px-3 py-1 m-5 rounded-2xl">
             TUTORIAL
           </button>
-          <div className="bg-white hover:bg-violet-600 hover:text-white ease-in-out w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto  mt-44 duration-300 cursor-pointer">
-            <div className="flex font-semibold">
-              <img src="" alt="" />
+          <div className="bg-white group-hover:bg-violet-600 group-hover:text-white ease-in-out duration-300 delay-100 w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto mt-40 cursor-pointer ">
+            <div className="flex font-semibold items-center gap-2">
+              <img src="/src/Assects/images/clock.png" className="w-5" alt="" />
               <p>Date</p>
-              <img src="" alt="" />
-              <p>comment</p>
+              <div className="flex font-semibold items-center py-1 gap-2">
+                <img
+                  src="/src/Assects/images/comments.png"
+                  alt=""
+                  className="w-5"
+                />
+                <p>comment</p>
+              </div>
             </div>
             <div className="text-xl font-bold">
               <p>TOP 10 Ui Ux Designers</p>
             </div>
           </div>
         </div>
-        <div className="image2 h-[350px] rounded-xl w-96 justify-center items-center">
+        {/* 3 */}
+        <div className=" group image2 h-[350px] rounded-xl w-96 justify-center items-center">
           <button className="text-white bg-blue-800 px-3 py-1 m-5 rounded-2xl">
             TUTORIAL
           </button>
-          <div className="bg-white hover:bg-violet-600 hover:text-white ease-in-out w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto  mt-44 duration-300 cursor-pointer">
-            <div className="flex font-semibold">
-              <img src="" alt="" />
+          <div className="bg-white group-hover:bg-violet-600 group-hover:text-white ease-in-out duration-300 delay-100 w-[350px] rounded-lg justify-center items-center px-5 py-5 m-auto mt-40 cursor-pointer ">
+            <div className="flex font-semibold items-center gap-2">
+              <img src="/src/Assects/images/clock.png" className="w-5" alt="" />
               <p>Date</p>
-              <img src="" alt="" />
-              <p>comment</p>
+              <div className="flex font-semibold items-center py-1 gap-2">
+                <img
+                  src="/src/Assects/images/comments.png"
+                  alt=""
+                  className="w-5"
+                />
+                <p>comment</p>
+              </div>
             </div>
             <div className="text-xl font-bold">
               <p>TOP 10 Ui Ux Designers</p>
