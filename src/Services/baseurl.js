74339205c3b0a4d864a166eff0e88f4f0a8f7092
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2024-11-30 17:08:22 +0530

    change base url

--- src/Services/baseurl.js
+++ src/Services/baseurl.js
@@ -1,3 +1,3 @@
-export const baseurl = "http://localhost:2000";
+// export const baseurl = "http://localhost:2000";
 
-// export const baseurl = "https://newportfolio-backend-ppdl.onrender.com/";
+export const baseurl = "https://newportfolio-backend-ppdl.onrender.com";
