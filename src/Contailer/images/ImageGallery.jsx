f22027caeb315781611d8f22e3da65ef5424a6f2
amarkumarworkcodesis <167982410+amarkumarworkcodesis@users.noreply.github.com>
2025-02-25 12:49:08 +0530

    added images

--- src/Contailer/images/ImageGallery.jsx
+++ src/Contailer/images/ImageGallery.jsx
@@ -1,28 +1,8 @@
-import React from 'react';
-import ImageCard from './image';  
-import musicplayer from '../../Assects/images/Screenshot 2024-12-21 162209.png';
-import AIsumm from '../../Assects/images/AI summ.png';
-import shirts from '../../Assects/images/shirts.png';
+import React from "react";
+import ImageCard from "./image";
+import { images } from "../../Data/DummyData";
 
 const ImageGallery = () => {
-  const images = [
-    {
-      url: musicplayer,
-      redirectUrl: '#', 
-      key: 'Music Player',
-    },
-    {
-      url: AIsumm,
-      redirectUrl: '#', 
-      key: 'AI Summary',
-    },
-    {
-      url: shirts,
-      redirectUrl: '#', 
-      key: 'Shirts',
-    },
-  ];
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-5">
       {images.map((image, index) => (
---
+++ src/Data/DummyData.js
@@ -0,0 +1,28 @@
+import musicplayer from "../Assects/images/Screenshot 2025-02-25 115123.png";
+import AIsumm from "../Assects/images/AI summ.png";
+import shirts from "../Assects/images/shirts.png";
+import Ecomm from "../Assects/images/Screenshot 2025-02-25 115012.png";
+
+export const images = [
+  {
+    url: musicplayer,
+    redirectUrl: "https://musicwebappplay.netlify.app/",
+    key: "Music Player",
+  },
+  {
+    url: Ecomm,
+    redirectUrl: "https://ecommak.netlify.app/",
+    key: "Ecomm",
+  },
+  {
+    url: AIsumm,
+    redirectUrl: "https://ai-summery.netlify.app/",
+    key: "AI Summary",
+  },
+  {
+    url: shirts,
+    redirectUrl: "https://tshirts123.netlify.app/",
+    key: "Shirts",
+  },
+ 
+];
